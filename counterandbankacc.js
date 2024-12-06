7260a9591ea3bf46e1934342eab78c876bf7f5b1
Gowsalya Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2024-12-07 00:30:52 +0530

    bank acc problem

--- counterandbankacc.js
+++ counterandbankacc.js
@@ -25,3 +25,26 @@ counter.increment();
 counter.increment();
 counter.decrement();
 counter.decrement();
+
+//branch code
+//Question 2: Simulating Private Variables with Closures
+function createBankAccount(){
+    return {
+        deposit : function(deposit){
+            let balance = 1000;
+            depo = balance+deposit;
+            console.log("Deposited Amount is:" + depo);
+        },
+        withdraw : function(withdraw){
+                 withdra = depo - withdraw;
+                 console.log("Withdraw Amount is :" + withdra);
+            if(withdra < 1000)
+                console.log("Insufficient Balance");
+        },
+    }
+}
+let ans = createBankAccount();
+ans.deposit(1000);
+ans.withdraw(500);
+ans.withdraw(1100);
+console.log(ans.balance);
